1c43c99006bdb21d0a71dbfeb903de7e21734a25
lotaviods <l.otavio9099@gmail.com>
2023-05-27 14:01:18 -0300

    feat: Setting background while loading user info

--- app/src/main/java/com/github/lotaviods/linkfatec/ui/application/navigation/ApplicationNavHost.kt
+++ app/src/main/java/com/github/lotaviods/linkfatec/ui/application/navigation/ApplicationNavHost.kt
@@ -1,18 +1,25 @@
 package com.github.lotaviods.linkfatec.ui.application.navigation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.github.lotaviods.linkfatec.R
 import com.github.lotaviods.linkfatec.model.User.Companion.fromJson
 import com.github.lotaviods.linkfatec.ui.login.LoginScreen
 import com.github.lotaviods.linkfatec.ui.login.viewmodel.LoginScreenViewModel
@@ -44,15 +51,26 @@ fun ApplicationNavHost(navController: NavHostController, startDestination: Strin
         }
         composable("loading") {
             Box(
-                contentAlignment = Alignment.Center,
                 modifier = Modifier
-                    .background(Color.White)
-                    .fillMaxWidth()
+                    .fillMaxSize()
             ) {
-                LinearProgressIndicator(
-                    modifier = Modifier.fillMaxWidth(),
-                    color = ThemeColor.Red
+                Image(
+                    painter = painterResource(id = R.drawable.cps_gov_background_image),
+                    contentDescription = "CPS_LOGO",
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentScale = ContentScale.FillHeight
                 )
+                Box(
+                    contentAlignment = Alignment.TopStart,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    LinearProgressIndicator(
+                        modifier = Modifier.fillMaxWidth(),
+                        color = ThemeColor.Red
+                    )
+                }
             }
         }
     }
